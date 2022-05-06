c2e87951d707a7c57c6fe4d0f040d0fb0d19242e
Wongs <32192450+wongzeon@users.noreply.github.com>
2022-05-06 17:43:12 +0800

    支持将查询到的备案信息写入到Excel表格文件
    V2.0 支持将查询到的备案信息写入到Excel表格文件

--- beian-info.py
+++ beian-info.py
@@ -1,211 +1,237 @@
 # -*- coding: utf-8 -*-
-import requests
-import hashlib
+import re
+import os
+import cv2
 import time
 import base64
-import cv2
-import os
+import hashlib
+import requests
+import openpyxl as xl
+from openpyxl.styles import Alignment
 
-os.environ['no_proxy'] = '*' #避免因系统代理设置导致请求失败
+os.environ['no_proxy'] = '*'
 
-while True:
-    #提前获取要查询的对象信息，以免Token失效（Token有效时间为3分钟）
+def query_base(): 
+    print("版本：V2.0 可用测试：2022-5-6\n")
     print("项目地址：https://github.com/wongzeon/ICP-Checker\n")
-    print("版本：V1.1 可用测试：2021-7-27\n")
-    print("以企业名称查询的，需要输入企业全称\n")
-    print("以域名查询的，请不要输入“http/www”等域名外的字符\n")
-    info = input("请输入要查询的备案信息，可以为公司名或域名：")
-    info_data = {
-        'pageNum':'',
-        'pageSize':'',
-        'unitName':info
-    }
-    #构造AuthKey
+    while True:
+        try:
+            info = input("输入公司名（全称，括号以英文括号输入）/ 域名查询备案信息：").replace(" ","").replace("https://www.","").replace("http://www.","").replace("http://","")
+            #过滤空值和特殊字符，只允许 - . () 分别用于域名和公司名
+            if info == "":
+                raise ValueError("InputNone")
+            info = re.sub("[^\\u4e00-\\u9fa5-A-Za-z0-9,-.()]","",info)
+            input_zh = re.compile(u'[\u4e00-\u9fa5]')
+            zh_match = input_zh.search(info)
+            if zh_match:
+                info_result = info
+            else:
+                #检测是否为可备案的域名类型（类型同步日期2022/01/06）
+                input_url = re.compile(r'([^.]+)(?:\.(?:GOV\.cn|ORG\.cn|AC\.cn|MIL\.cn|NET\.cn|EDU\.cn|COM\.cn|BJ\.cn|TJ\.cn|SH\.cn|CQ\.cn|HE\.cn|SX\.cn|NM\.cn|LN\.cn|JL\.cn|HL\.cn|JS\.cn|ZJ\.cn|AH\.cn|FJ\.cn|JX\.cn|SD\.cn|HA\.cn|HB\.cn|HN\.cn|GD\.cn|GX\.cn|HI\.cn|SC\.cn|GZ\.cn|YN\.cn|XZ\.cn|SN\.cn|GS\.cn|QH\.cn|NX\.cn|XJ\.cn|TW\.cn|HK\.cn|MO\.cn|REN|WANG|CITIC|TOP|SOHU|XIN|COM|NET|CLUB|XYZ|VIP|SITE|SHOP|INK|INFO|MOBI|RED|PRO|KIM|LTD|GROUP|BIZ|AUTO|LINK|WORK|LAW|BEER|STORE|TECH|FUN|ONLINE|ART|DESIGN|WIKI|LOVE|CENTER|VIDEO|SOCIAL|TEAM|SHOW|COOL|ZONE|WORLD|TODAY|CITY|CHAT|COMPANY|LIVE|FUND|GOLD|PLUS|GURU|RUN|PUB|EMAIL|LIFE|CO|FASHION|FIT|LUXE|YOGA|BAIDU|CLOUD|HOST|SPACE|PRESS|WEBSITE|ARCHI|ASIA|BIO|BLACK|BLUE|GREEN|LOTTO|ORGANIC|PET|PINK|POKER|PROMO|SKI|VOTE|VOTO|ICU))',flags=re.IGNORECASE)
+                info_result = input_url.search(info)
+                if info_result is None:
+                    if info.split(".")[0] == "":
+                        raise ValueError("OnlyDomainInput")
+                    raise ValueError("ValidType")
+                else:
+                    info_result = info_result.group()
+            info_data = {'pageNum':'1','pageSize':'40','unitName':info_result}
+            return info_data
+        except ValueError as e:
+            if str(e) == 'InputNone' or str(e) == 'OnlyDomainInput':
+                print("\n ************** 请正确输入域名 **************\n")
+            else:
+                print("\n*** 该域名不支持备案，请查阅：http://xn--fiq8ituh5mn9d1qbc28lu5dusc.xn--vuq861b/ ***\n")
+
+def get_cookies():
+    cookie_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32'}
+    err_num = 0
+    while err_num < 3:
+        try:
+            cookie = requests.utils.dict_from_cookiejar(requests.get('https://beian.miit.gov.cn/',headers=cookie_headers).cookies)['__jsluid_s']
+            return cookie
+        except:
+            err_num += 1
+            time.sleep(3)
+    return -1
+
+def get_token():
     timeStamp = int(round(time.time()*1000))
     authSecret = 'testtest' + str(timeStamp)
     authKey = hashlib.md5(authSecret.encode(encoding='UTF-8')).hexdigest()
-    #获取Cookie
-    cookie_headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36 Edg/90.0.818.42'
-    }
-    cookie = requests.utils.dict_from_cookiejar(requests.get('https://beian.miit.gov.cn/',headers=cookie_headers).cookies)['__jsluid_s']
-    #请求获取Token
-    t_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/auth'
-    t_headers = {
-        'Host': 'hlwicpfwc.miit.gov.cn',
-        'Connection': 'keep-alive',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
-        'Accept': '*/*',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46',
-        'Origin': 'https://beian.miit.gov.cn',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://beian.miit.gov.cn/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Cookie': '__jsluid_s=' + cookie
-    }
-    data = {
-        'authKey': authKey,
-        'timeStamp': timeStamp
-    }
-    t_response = requests.post(t_url,data=data,headers=t_headers)
+    auth_data = {'authKey': authKey,'timeStamp': timeStamp}
+    url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/auth'
     try:
-        get_token = t_response.json()['params']['bussiness']
+        t_response = requests.post(url=url,data=auth_data,headers=base_header).json()
+        token = t_response['params']['bussiness']
     except:
-        print('\n'"请求被禁止，请稍后或更换头部与IP后再试，状态码：",t_response.status_code)
-        break
-    #获取验证图像、UUID
-    p_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/image/getCheckImage'
-    p_headers = {
-        'Host': 'hlwicpfwc.miit.gov.cn',
-        'Connection': 'keep-alive',
-        'Content-Length': '0',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
-        'Accept': 'application/json, text/plain, */*',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46',
-        'token': get_token,
-        'Origin': 'https://beian.miit.gov.cn',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://beian.miit.gov.cn/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Cookie': '__jsluid_s=' + cookie
-    }
-    p_request = requests.post(p_url,data='',headers=p_headers)
+        return -1
+    return token
+
+def get_check_pic(token):
+    url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/image/getCheckImage'
+    base_header['Accept'] = 'application/json, text/plain, */*'
+    base_header.update({'Content-Length': '0','token':token})
     try:
-        p_uuid = p_request.json()['params']['uuid']
-        big_image = p_request.json()['params']['bigImage']
-        small_image = p_request.json()['params']['smallImage']
-    except KeyError:
-        print("请重试，请求状态码：",p_request.status_code)
+        p_request = requests.post(url=url,data='',headers=base_header).json()
+        p_uuid = p_request['params']['uuid']
+        big_image = p_request['params']['bigImage']
+        small_image = p_request['params']['smallImage']
+    except:
+        return -1
     #解码图片，写入并计算图片缺口位置
     with open('bigImage.jpg','wb') as f:
         f.write(base64.b64decode(big_image))
-        f.close()
     with open('smallImage.jpg','wb') as f:
         f.write(base64.b64decode(small_image))
-        f.close()
     background_image = cv2.imread('bigImage.jpg',cv2.COLOR_GRAY2RGB)
     fill_image = cv2.imread('smallImage.jpg',cv2.COLOR_GRAY2RGB)
-    background_image_canny = cv2.Canny(background_image, 100, 200)
-    fill_image_canny = cv2.Canny(fill_image, 100, 300)
     position_match = cv2.matchTemplate(background_image, fill_image, cv2.TM_CCOEFF_NORMED)
-    min_val,max_val,min_loc,max_loc = cv2.minMaxLoc(position_match)
-    position = max_loc
-    mouse_length = position[0]+1
+    max_loc = cv2.minMaxLoc(position_match)[3][0]
+    mouse_length = max_loc+1
     os.remove('bigImage.jpg')
     os.remove('smallImage.jpg')
-    #通过拼图验证，获取sign
+    check_data = {'key':p_uuid,'value':mouse_length}
+    return check_data
+
+def get_sign(check_data,token):
     check_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/image/checkImage'
-    check_headers = {
-        'Host': 'hlwicpfwc.miit.gov.cn',
-        'Accept': 'application/json, text/plain, */*',
-        'Connection': 'keep-alive',
-        'Content-Length': '60',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36 Edg/90.0.818.42',
-        'token': get_token,
-        'Content-Type': 'application/json',
-        'Origin': 'https://beian.miit.gov.cn',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://beian.miit.gov.cn/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Cookie': '__jsluid_s=' + cookie
-    }
-    check_data = {
-        'key':p_uuid,
-        'value':mouse_length
-    }
-    check_request = requests.post(check_url,json=check_data,headers=check_headers)
+    base_header.update({'Content-Length': '60','token':token,'Content-Type': 'application/json'})
     try:
-        sign = check_request.json()['params']
-    except Exception:
-        print('\n'"请求被禁止，请稍后或更换头部与IP后再试，状态码：",check_request.status_code)
-        break
-    #获取备案信息
+        pic_sign = requests.post(check_url,json=check_data,headers=base_header).json()
+        sign = pic_sign['params']
+    except:
+        return -1
+    return sign
+
+def get_beian_info(info_data,p_uuid,token,sign):
+    domain_list = []
     info_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/icpAbbreviateInfo/queryByCondition'
-    info_headers = {
-        'Host': 'hlwicpfwc.miit.gov.cn',
-        'Connection': 'keep-alive',
-        'Content-Length': '78',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36 Edg/90.0.818.42',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json, text/plain, */*',
-        'uuid': p_uuid,
-        'token': get_token,
-        'sign': sign,
-        'Origin': 'https://beian.miit.gov.cn',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://beian.miit.gov.cn/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Cookie': '__jsluid_s=' + cookie
-    }
-    info_request = requests.post(info_url,json=info_data,headers=info_headers)
-    domain_total = info_request.json()['params']['total']
-    page_total = info_request.json()['params']['lastPage']
-    page_size = info_request.json()['params']['pageSize']
-    start_row = info_request.json()['params']['startRow']
-    end_row = info_request.json()['params']['endRow']
-    os.system('cls')
-    print("\n查询对象",info,"共有",domain_total,"个备案域名",'\n')
-    print("域名具体信息如下：")
-    for i in range(1,page_total+1):
-        for k in range(start_row,end_row+1):
-            info_base = info_request.json()['params']['list'][k]
-            domain_name = info_base['domain']
-            domain_type = info_base['natureName']
-            domain_licence = info_base['mainLicence']
-            domain_web_licence = info_base['serviceLicence']
-            domain_status = info_base['limitAccess']
-            domain_approve_date = info_base['updateRecordTime']
-            domain_owner = info_base['unitName']
-            try:
-                domain_content_approved = info_base['contentTypeName']
-                if not bool(domain_content_approved):
+    base_header.update({'Content-Length': '78','uuid': p_uuid,'token': token,'sign': sign})
+    try:
+        beian_info = requests.post(url=info_url,json=info_data,headers=base_header).json()
+        domain_total = beian_info['params']['total']
+        page_total = beian_info['params']['lastPage']
+        end_row = beian_info['params']['endRow']
+        info = info_data['unitName']
+        print(f"\n查询对象：{info} 共有 {domain_total} 个已备案域名\n")
+        for i in range(0,page_total):
+            print(f"正在查询第{i+1}页……\n")
+            for k in range(0,end_row+1):
+                info_base = beian_info['params']['list'][k]
+                domain_name = info_base['domain']
+                domain_type = info_base['natureName']
+                domain_licence = info_base['mainLicence']
+                website_licence = info_base['serviceLicence']
+                domain_status = info_base['limitAccess']
+                domain_approve_date = info_base['updateRecordTime']
+                domain_owner = info_base['unitName']
+                try:
+                    domain_content_approved = info_base['contentTypeName']
+                    if domain_content_approved == "":
+                        domain_content_approved = "无"
+                except KeyError:
                     domain_content_approved = "无"
-            except KeyError:
-                domain_content_approved = "无"
-            print("\n域名主办方：",domain_owner,'\n')
-            print("域名：",domain_name,'\n')
-            print("备案许可证号：",domain_licence,'\n')
-            print("网站备案号：",domain_web_licence,'\n')
-            print("域名类型：",domain_type,'\n')
-            print("网站前置审批项：",domain_content_approved,'\n')
-            print("是否限制接入：",domain_status,'\n')
-            print("审核通过日期：",domain_approve_date,'\n')
-        info_data_page = {
-            'pageNum':i+1,
-            'pageSize':'10',
-            'unitName':info
+                row_data = domain_owner,domain_name,domain_licence,website_licence,domain_type,domain_content_approved,domain_status,domain_approve_date
+                domain_list.append(row_data)
+            info_data = {'pageNum':i+2,'pageSize':'40','unitName':info}
+            if beian_info['params']['isLastPage'] is True:
+                break
+            else:
+                beian_info = requests.post(info_url,json=info_data,headers=base_header).json()
+                end_row = beian_info['params']['endRow']
+                time.sleep(3)
+    except:
+        return domain_list
+    return domain_list
+
+def data_saver(domain_list):
+    print(f"查询结果如下:\n\n{domain_list}\n")
+    row = len(domain_list)
+    if row == 1:
+        row = 0
+    elif row == 0:
+        return print("所查域名无备案")
+    #Windows获取桌面路径，将表格保存到桌面，其他系统默认保存到/home/文件夹下
+    if os.name == "nt":
+        import winreg
+        subkey = r'Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders'
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,subkey,0)
+        desktop_path = str(winreg.QueryValueEx(key, "Desktop")[0]).replace('\\','/') + "/"
+        file_path = "%s备案信息.xlsx"%desktop_path
+    else:
+        file_path = '/home/备案信息.xlsx'
+    #存在对应文件，则读取表格追加写入，不存在则创建，并设置表格的标题、列宽、冻结窗格、文字布局等格式
+    if os.path.exists(file_path):
+        wb = xl.load_workbook(file_path)
+        ws = wb['备案信息']
+        max_row = ws.max_row
+        start = max_row+1
+        end = row + start
+        after_title = 0
+    else:
+        wb = xl.Workbook()
+        ws = wb.active
+        ws.title = "备案信息"
+        title_list = ['域名主办方','域名','备案许可证号','网站备案号','域名类型','网站前置审批项','是否限制接入','审核通过日期']
+        for i in range(0,8):
+            ws.cell(1,i+1).value=title_list[i]
+        col_width = {'A':45,'B':40,'C':22,'D':24,'E':9,'F':15,'G':13,'H':21}
+        for k,v in col_width.items():
+            ws.column_dimensions[k].width = v
+        ws.freeze_panes = 'A2'
+        start = 0
+        end = row
+        after_title = 2
+    #写入查询数据
+    for j in range(start,end+1):
+        for k in range(0,8):
+            try:
+                print(j,k,domain_list[j-start][k])
+                ws.cell(j+after_title,k+1).value = domain_list[j-start][k]
+            except:
+                continue
+    #垂直居中
+    for row in range(ws.max_row):
+        for col in range(ws.max_column):
+            ws.cell(row+1,col+1).alignment = Alignment(horizontal='center',vertical='center')
+    wb.save(file_path)
+    print("查询结果保存在：%s\n"%file_path)
+    return 'OK'
+
+def main():
+    cookie = get_cookies()
+    while True:
+        info = query_base()
+        global base_header
+        base_header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32',
+            'Origin': 'https://beian.miit.gov.cn',
+            'Referer': 'https://beian.miit.gov.cn/',
+            'Cookie': f'__jsluid_s={cookie}'
         }
-        if info_data_page['pageNum'] > page_total:
-            print("查询完毕",'\n')
-            break
-        else:
-            info_request = requests.post(info_url,json=info_data_page,headers=info_headers)
-            start_row = info_request.json()['params']['startRow']
-            end_row = info_request.json()['params']['endRow']
-            time.sleep(3)
-    os.system('pause')
+        try:
+            if cookie != -1:
+                token = get_token()
+                if token != -1:
+                    check_data = get_check_pic(token)
+                    if check_data != -1:
+                        sign = get_sign(check_data,token)
+                        p_uuid = check_data['key']
+                        if sign != -1:
+                            domain_list = get_beian_info(info,p_uuid,token,sign)
+                            data_saver(domain_list)
+                        else:
+                            raise ValueError("获取Sign遇到错误，请重试！")
+                    else:
+                        raise ValueError("计算图片缺口位置错误，请重试！")
+                else:
+                    raise ValueError("获取Token失败，如频繁失败请关闭程序后等待几分钟再试！")
+            else:
+                cookie = get_cookies()
+                raise ValueError("获取Cookie失败，请重试！")
+        except Exception as e:
+            print(e,'\n')
+
+if __name__ == '__main__':
+    main()
