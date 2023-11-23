f8e49f41b954fe7095e7abc40bdb449ea8a71579
Wongs <32192450+wongzeon@users.noreply.github.com>
2023-11-23 11:54:49 +0800

    整体重写

--- ICP-Checker.py
+++ ICP-Checker.py
@@ -1,108 +1,110 @@
-import re
+# -*- coding: utf-8 -*-
 import os
-import time
-import hashlib
-import requests
-from requests import utils
-import openpyxl as xl
+import re
+from requests import get, post
+from time import sleep, time
+from hashlib import md5
+from openpyxl import load_workbook
+from openpyxl.workbook import Workbook
 from openpyxl.styles import Alignment
 
-os.environ['no_proxy'] = '*'
+class get_require_info(object):
+    """
+    获取必要的Cookie、Token，以及各类的POST请求由这里的post_tool()完成，
+    """
+    base_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69',
+                'Origin': 'https://beian.miit.gov.cn', 'Referer': 'https://beian.miit.gov.cn/'}
 
+    def __init__(self) -> None:
+        self.base_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/'
+        self.auth_url = self.base_url + 'auth' # 返回token
+        self.image_url = self.base_url + 'image/getCheckImagePoint' # 返回Secretkey、uuid
+        self.check_url = self.base_url + 'image/checkImage' # 返回Sign，传值里的token是uuid
+        self.query_url = self.base_url + 'icpAbbreviateInfo/queryByCondition' # 标头需要：cookie、token、sign、uuid
 
-def query_base():
-    print("版本：V2.1.9 可用测试：2023-9-25\n")
-    print("项目地址：https://github.com/wongzeon/ICP-Checker\n")
-    while True:
+    def post_tool(self, url, data, json, header):
         try:
-            info = (
-                input("请完整输入公司全称 / 域名以查询备案信息：\n\n")
-                .replace(" ", "")
-                .replace("https://www.", "")
-                .replace("http://www.", "")
-                .replace("http://", "")
-            )
-            # 过滤空值和特殊字符，只允许 - —《》. () 分别用于域名和公司名
-            if info == "":
-                raise ValueError("InputNone")
-            info = re.sub("[^\\u4e00-\\u9fa5-A-Za-z0-9,-.()《》—（）]", "", info)
-            input_zh = re.compile(u'[\u4e00-\u9fa5]')
-            zh_match = input_zh.search(info)
-            if zh_match:
-                info_result = info
+            response = post(url, data=data, json=json, headers=header, timeout=2)
+            response_code = response.json()['code']
+            response_msg = response.json()['success']
+            if response_code == 200 and response_msg is True:
+                return response
+            elif response_code == 200 and response_msg is False: # 验证错误，需要重新获取验证图片
+                return 'pic_check_fail'
+            elif response_code == 401: # Token 过期
+                print(f"{' 刷新Token ':-^57}")
+                return self.get_token()
+            elif response_code == (500 or 501):
+                return 'server_error'
+            elif response.status_code == 403: # WAF 触发，只有这个会正确返回状态码，其他的错误状态码响应的是200
+                print(f"{' 触发防火墙了，过几分钟再试吧 ':*^50}")
+                exit()
+        except Exception as err:
+            return f'error:{err}'
+
+    def get_cookie(self):
+        cookie = get(url=self.base_header['Origin'], headers={'User-Agent':self.base_header['User-Agent']}, timeout=1)
+        self.base_header.update({'Cookie': cookie.headers['Set-Cookie'].split(';')[0]})
+
+    def get_token(self):
+        print(f"\n{' 获取Token中，服务器响应时快时慢 ':-^46}")
+        time_stamp = round(time() * 1000)
+        auth_key = md5(f'testtest{time_stamp}'.encode('utf-8')).hexdigest()
+        auth_data = {'authKey': auth_key, 'timeStamp': time_stamp}
+        token_request = self.post_tool(self.auth_url, auth_data, "", self.base_header)
+        token_result = token_request.json()['params']['bussiness']
+        self.base_header.update({'Token': token_result, 'Accept':'application/json'})
+        print(f"\n{' 已获得Token ':-^56}\n")
+
+class file_tool(object):
+    """
+    文件处理，主要为批量做预留，减少重复代码
+    """
+    def __init__(self) -> None:
+        pass
+        
+    def file_writer(self, file_path, file_name, file, wr_mode, wr_type):
+        with open(f'{file_path}/{file_name}', wr_mode) as f:
+            if wr_type == 'write':
+                f.write(file)
             else:
-                # 检测是否为可备案的域名类型（类型同步日期2022/01/06）
-                # TODO: 建议移除域名类型检测, 维护不及时且部分留存域名类型较为特殊, 包括 IP 备案.
-                input_url = re.compile(
-                    r'([^.]+)(?:\.(?:GOV\.cn|ORG\.cn|AC\.cn|MIL\.cn|NET\.cn|EDU\.cn|COM\.cn|BJ\.cn|TJ\.cn|SH\.cn|CQ\.cn|HE\.cn|SX\.cn|NM\.cn|LN\.cn|JL\.cn|HL\.cn|JS\.cn|ZJ\.cn|AH\.cn|FJ\.cn|JX\.cn|SD\.cn|HA\.cn|HB\.cn|HN\.cn|GD\.cn|GX\.cn|HI\.cn|SC\.cn|GZ\.cn|YN\.cn|XZ\.cn|SN\.cn|GS\.cn|QH\.cn|NX\.cn|XJ\.cn|TW\.cn|HK\.cn|MO\.cn|cn|REN|WANG|CITIC|TOP|SOHU|XIN|COM|NET|CLUB|XYZ|VIP|SITE|SHOP|INK|INFO|MOBI|RED|PRO|KIM|LTD|GROUP|BIZ|AUTO|LINK|WORK|LAW|BEER|STORE|TECH|FUN|ONLINE|ART|DESIGN|WIKI|LOVE|CENTER|VIDEO|SOCIAL|TEAM|SHOW|COOL|ZONE|WORLD|TODAY|CITY|CHAT|COMPANY|LIVE|FUND|GOLD|PLUS|GURU|RUN|PUB|EMAIL|LIFE|CO|FASHION|FIT|LUXE|YOGA|BAIDU|CLOUD|HOST|SPACE|PRESS|WEBSITE|ARCHI|ASIA|BIO|BLACK|BLUE|GREEN|LOTTO|ORGANIC|PET|PINK|POKER|PROMO|SKI|VOTE|VOTO|ICU|LA|CC))',
-                    flags=re.IGNORECASE)
-                info_result = input_url.search(info)
-                if info_result is None:
-                    if info.split(".")[0] == "":
-                        raise ValueError("OnlyDomainInput")
-                    raise ValueError("ValidType")
-                else:
-                    info_result = info_result.group()
-            info_data = {'pageNum': '1', 'pageSize': '40', 'serviceType': 1, 'unitName': info_result}
-            return info_data
-        except ValueError as e:
-            if str(e) == 'InputNone' or str(e) == 'OnlyDomainInput':
-                print("\n ************** 请正确输入域名 **************\n")
-            else:
-                print("\n*** 该域名不支持备案，请查阅：http://xn--fiq8ituh5mn9d1qbc28lu5dusc.xn--vuq861b/ ***\n")
+                self.file_content = f.read()
+                return self.file_content
 
+class query_info(get_require_info, file_tool):
+    """
+    输入过滤，只对中文输入做过滤，英文域名由于有些域名不在可备案列表内也能备案，所以不做过滤了
+    """
+    domain_result_list = []
+    def __init__(self) -> None:
+        super().__init__()
 
-def get_cookies():
-    cookie_headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32'}
-    err_num = 0
-    while err_num < 3:
-        try:
-            cookie = utils.dict_from_cookiejar(
-                requests.get(
-                    'https://beian.miit.gov.cn/',
-                    headers=cookie_headers,
-                    timeout=(3.06, 27)).cookies
-            )['__jsluid_s']
-            return cookie
-        except:
-            err_num += 1
-            time.sleep(3)
-    return -1
+    def regular_input(self):
+        print(f"{' 输入 域名/备案号/完整公司名 以查询备案信息 ':-^40}\n")
+        info_input = input('查询对象：').replace(' ', '').replace('https://www.', '').replace('http://www.', '').replace('http://', '').replace('www.', '')
+        if info_input == '':
+             print(f"\n{' 请输入正确域名 ':*^52}\n")
+             return -1
+        elif '\u4e00' <= info_input <= '\u9fff':
+            input_result = re.sub('[^\\u4e00-\\u9fff-A-Za-z0-9,-.()《》—（）]', "", info_input) # 中文输入混入的特殊符号，只允许：-—《》（）().
+        else:
+            input_result = info_input
+        self.query_text = {'pageNum': 1, 'pageSize': 40, 'serviceType': 1, 'unitName': input_result} #serviceType: 1:网站 6:APP 7:小程序 8:快应用
 
-
-def get_token():
-    timeStamp = round(time.time() * 1000)
-    authSecret = 'testtest' + str(timeStamp)
-    authKey = hashlib.md5(authSecret.encode(encoding='UTF-8')).hexdigest()
-    auth_data = {'authKey': authKey, 'timeStamp': timeStamp}
-    url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/auth'
-    try:
-        t_response = requests.post(url=url, data=auth_data, headers=base_header, timeout=(3.06, 27)).json()
-        token = t_response['params']['bussiness']
-    except:
-        return -1
-    return token
-
-
-def get_beian_info(info_data, token):
-    domain_list = []
-    info_url = 'https://hlwicpfwc.miit.gov.cn/icpproject_query/api/icpAbbreviateInfo/queryByCondition'
-    base_header.update({'token': token})
-    try:
-        beian_info = requests.post(url=info_url, json=info_data, headers=base_header, timeout=(3.06, 27)).json()
-        if not beian_info["success"]:
-            print(f'请求错误: CODE {beian_info["code"]} MSG {beian_info["msg"]}')
-            return domain_list
-        domain_total = beian_info['params']['total']
-        page_total = beian_info['params']['lastPage']
-        end_row = beian_info['params']['endRow']
-        info = info_data['unitName']
-        print(f"\n查询对象：{info} 共有 {domain_total} 个已备案域名\n")
-        for i in range(0, page_total):
-            print(f"正在查询第{i + 1}页……\n")
+    def query_information(self):
+        print(f"\n{' 查询中 ':-^56}")
+        icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+        total_domains = icp_info['total']
+        total_pages = icp_info['lastPage']
+        end_row = icp_info['endRow']
+        if total_domains == 0:
+            print(f"\n{' '+ self.query_text['unitName'] + ' 没有备案信息 ':*^40}\n")
+            return -1
+        print(f"\n{' '+ self.query_text['unitName'] + ' 共有 ' + str(total_domains) + ' 个备案域名 ':*^40}\n")
+        for i in range(total_pages):
+            print(f"{' 获取第' + str(i+1) + '页信息 ':-^53}\n")
             for k in range(0, end_row + 1):
-                info_base = beian_info['params']['list'][k]
+                info_base = icp_info['list'][k]
                 domain_name = info_base['domain']
                 domain_type = info_base['natureName']
                 domain_licence = info_base['mainLicence']
@@ -117,118 +119,87 @@ def get_beian_info(info_data, token):
                 except KeyError:
                     domain_content_approved = "无"
                 row_data = domain_owner, domain_name, domain_licence, website_licence, domain_type, domain_content_approved, domain_status, domain_approve_date
-                domain_list.append(row_data)
-            info_data = {'pageNum': i + 2, 'pageSize': '40', 'serviceType': 1, 'unitName': info}
-            if beian_info['params']['isLastPage'] is True:
+                self.domain_result_list.append(row_data)
+            self.query_text.update({'pageNum': i + 2})
+            if icp_info['isLastPage'] is True:
                 break
             else:
-                beian_info = requests.post(info_url, json=info_data, headers=base_header, timeout=(3.06, 27)).json()
-                end_row = beian_info['params']['endRow']
-                time.sleep(3)
-    except Exception as e:
-        print(f"意外错误: {e}")
-        return domain_list
-    return domain_list
+                icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+                end_row = icp_info['endRow']
+                sleep(3) # 翻页需要慢一点，否则大概率被WAF拦截
 
-
-def data_saver(domain_list):
+class excel_tool(query_info):
     """
-    打印最终结果，并保存数据至Excel表格，同时调整表格格式。
+    保存结果到Excel文件，注意执行时需关闭“备案信息.xlsx”，否则会保存失败
     """
-    # 计算需要写入表格的总行数，如果是空列表，即代表该域名没有备案信息，也有可能是获取信息失败了
-    total_row = len(domain_list)
-    if total_row == 1:
-        total_row = 0
-    elif total_row == 0:
-        return print("所查域名无备案\n")
-    print(f"查询结果如下:\n\n{domain_list}\n")
-    # Windows获取桌面路径，将表格保存到桌面，其他系统默认保存到/home/文件夹下
-    if os.name == "nt":
-        import winreg
-        # 用户更改过桌面路径，则需获取User Shell Folders才能获取到准确的桌面路径，否则不会保存到实际的桌面
-        subkey = r'Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders'
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, subkey, 0)
-        desktop_raw = str(winreg.QueryValueEx(key, "Desktop")[0])
-        if desktop_raw == "%USERPROFILE%\Desktop":
-            # 此时情况为用户未更改过桌面路径，则需获取系统默认路径
-            subkey = r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, subkey, 0)
-            desktop_raw = str(winreg.QueryValueEx(key, "Desktop")[0])
-        desktop_path = desktop_raw.replace('\\', '/') + "/"
-        file_path = f"{desktop_path}备案信息.xlsx"
-    else:
-        file_path = '/home/备案信息.xlsx'
-    # 存在对应文件，则读取表格追加写入，不存在则创建，并设置表格的标题、列宽、冻结窗格、文字布局等格式
-    if os.path.exists(file_path):
-        wb = xl.load_workbook(file_path)
-        ws = wb['备案信息']
-        max_row = ws.max_row
-        start = max_row + 1
-        total_row = total_row + start
-        after_title = 0
-    else:
-        wb = xl.Workbook()
-        ws = wb.active
-        ws.title = "备案信息"
-        title_list = ['域名主办方', '域名', '备案许可证号', '网站备案号', '域名类型', '网站前置审批项', '是否限制接入',
-                      '审核通过日期']
-        for i in range(0, 8):
-            ws.cell(1, i + 1).value = title_list[i]
-        col_width = {'A': 45, 'B': 40, 'C': 22, 'D': 24, 'E': 9, 'F': 15, 'G': 13, 'H': 21}
-        for k, v in col_width.items():
-            ws.column_dimensions[k].width = v
-        ws.freeze_panes = 'A2'
-        start = 0
-        after_title = 2
-    # 写入查询数据
-    for j in range(start, total_row + 1):
-        for k in range(0, 8):
-            try:
-                ws.cell(j + after_title, k + 1).value = domain_list[j - start][k]
-            except:
-                continue
-    # 垂直居中
-    for row in range(ws.max_row):
-        for col in range(ws.max_column):
-            ws.cell(row + 1, col + 1).alignment = Alignment(horizontal='center', vertical='center')
-    try:
-        wb.save(file_path)
-    except PermissionError:
-        print("** 备案信息登记表格已打开，无法写入文件。如需写入，请关闭文件后重新执行！ **\n")
-        return -1
-    print(f"查询结果保存在：{file_path}\n")
-    return 'OK'
+    def __init__(self) -> None:
+        if os.name == 'nt':
+            from winreg import OpenKey, QueryValueEx, HKEY_CURRENT_USER
+            key = r'Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' # 修改过桌面路径的注册表
+            target_key = OpenKey(HKEY_CURRENT_USER, key, 0)
+            desktop_path = str(QueryValueEx(target_key, 'Desktop')[0])
+            if desktop_path == r'%USERPROFILE%\Desktop':
+                desktop_path = os.path.join(os.path.expanduser('~'),"Desktop")
+        else:
+            desktop_path = f'/home/{os.getlogin()}'
+            if not os.path.exists(desktop_path):
+                os.mkdir(desktop_path)
+        self.excel_file_path = desktop_path.replace('\\', '/') + '/备案信息.xlsx'
+        super().__init__()
 
-
-def main():
-    print("获取Cookie中，速度取决于网站响应，请等待……\n")
-    cookie = get_cookies()
-    while True:
-        info = query_base()
+    def set_format(self):
+        list_row = len(self.domain_result_list)
+        if os.path.exists(self.excel_file_path):
+            self.work_book = load_workbook(self.excel_file_path)
+            self.work_sheet = self.work_book['备案信息']
+            self.start = self.work_sheet.max_row + 1 # 从文件最后一行的后一行开始追加写入
+            self.total_row = list_row + self.start
+            self.after_title = 0
+        else:
+            self.work_book = Workbook()
+            self.work_sheet = self.work_book.active
+            self.work_sheet.title = "备案信息" 
+            self.work_sheet.freeze_panes = 'A2' # 冻结窗格
+            col_title_list = ['域名主办方', '域名', '备案许可证号', '网站备案号', '域名类型', '网站前置审批项', '是否限制接入', '审核通过日期']
+            col_width = {'A': 45, 'B': 30, 'C': 22, 'D': 24, 'E': 9, 'F': 41, 'G': 13, 'H': 21}
+            for i in range(8):
+                self.work_sheet.cell(1, i + 1).value = col_title_list[i] # 写入表头
+            for key, value in col_width.items():
+                self.work_sheet.column_dimensions[key].width = value # 设定列宽
+            self.start = 0
+            self.after_title = 2
+            self.total_row = list_row
+        self.write_data()
+        
+    def write_data(self):
+        print(f"{' 正在录入信息到表格 ':-^50}\n")
+        for i in range(self.start, self.total_row):
+            for j in range(8):
+                self.work_sheet.cell(i + self.after_title, j + 1).value = self.domain_result_list[i - self.start][j] # 写入数据，对应domain_reuslt_list的数据，结构是：[(1,2,3,4,5,6,7,8),(),()……]
+        for row in range(self.work_sheet.max_row):
+            for col in range(self.work_sheet.max_column):
+                self.work_sheet.cell(row + 1, col + 1).alignment = Alignment(horizontal='center', vertical='center') # 居中对齐
         try:
-            global base_header
-            base_header = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32',
-                'Origin': 'https://beian.miit.gov.cn',
-                'Referer': 'https://beian.miit.gov.cn/',
-                'Cookie': f'__jsluid_s={cookie}'
-            }
-            # -1代表对应步骤失败了，不是-1则正常执行下一步
-            if cookie != -1:
-                token = get_token()
-                print("\n获取Token，请等待……\n")
-                if token != -1:
-                    print("已获取到Token，查询中，速度取决于网站响应，请等待……")
-                    domain_list = get_beian_info(info, token)
-                    data_saver(domain_list)
-                else:
-                    raise ValueError("获取Token失败，如频繁失败请关闭程序后等待几分钟再试！")
-            else:
-                cookie = get_cookies()
-                raise ValueError("获取Cookie失败，请重试！")
-        except Exception as e:
-            print(f'{e}\n')
-
+            self.work_book.save(self.excel_file_path)
+        except PermissionError:
+            return print("** 备案信息登记表格已打开，无法写入文件。如需写入，请关闭文件后重新执行！ **\n")
+        return print(f"{' 查询结果保存在：' + str(self.excel_file_path) + ' ':*^47}\n")
 
 if __name__ == '__main__':
-    main()
+   """
+   各类可预期的报错，返回为-1
+   """
+   os.environ['no_proxy'] = '*'
+   query_target = query_info() 
+   requirement_info = get_require_info() 
+   save_data = excel_tool() 
+   print(f"\n{' 项目地址：https://github.com/wongzeon/ICP-Checker ':-^54}\n")
+   print(f"{' V2.2.0 2023/11/23 仅用于学习研究，不得用于商业/非法用途 ':-^41}")
+   requirement_info.get_cookie() 
+   token = requirement_info.get_token() 
+   exe_num = 0
+   while exe_num <= 2:
+        if query_target.regular_input() != -1: 
+            if query_target.query_information() != -1: 
+                save_data.set_format()
+        exe_num += 1 # 控制循环次数han
