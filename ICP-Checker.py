0143842bdad6137352135c3a90be36d6396be2fa
Wongs <32192450+wongzeon@users.noreply.github.com>
2023-11-23 14:04:28 +0800

    对输入域名进行简单的过滤，修改报错提示
    域名需要包含.字符

--- ICP-Checker.py
+++ ICP-Checker.py
@@ -8,7 +8,7 @@ from openpyxl import load_workbook
 from openpyxl.workbook import Workbook
 from openpyxl.styles import Alignment
 
-class get_require_info(object):
+class get_required_info(object):
     """
     获取必要的Cookie、Token，以及各类的POST请求由这里的post_tool()完成，
     """
@@ -24,7 +24,7 @@ class get_require_info(object):
 
     def post_tool(self, url, data, json, header):
         try:
-            response = post(url, data=data, json=json, headers=header, timeout=2)
+            response = post(url, data=data, json=json, headers=header, timeout=10)
             response_code = response.json()['code']
             response_msg = response.json()['success']
             if response_code == 200 and response_msg is True:
@@ -32,29 +32,29 @@ class get_require_info(object):
             elif response_code == 200 and response_msg is False: # 验证错误，需要重新获取验证图片
                 return 'pic_check_fail'
             elif response_code == 401: # Token 过期
-                print(f"{' 刷新Token ':-^57}")
-                return self.get_token()
+                print(f"{' 刷新Token请重试 ':-^54}")
+                self.get_token()
+                return -1
             elif response_code == (500 or 501):
                 return 'server_error'
             elif response.status_code == 403: # WAF 触发，只有这个会正确返回状态码，其他的错误状态码响应的是200
                 print(f"{' 触发防火墙了，过几分钟再试吧 ':*^50}")
                 exit()
         except Exception as err:
-            return f'error:{err}'
+            return print(f'error:{err}')
 
     def get_cookie(self):
         cookie = get(url=self.base_header['Origin'], headers={'User-Agent':self.base_header['User-Agent']}, timeout=1)
         self.base_header.update({'Cookie': cookie.headers['Set-Cookie'].split(';')[0]})
 
     def get_token(self):
-        print(f"\n{' 获取Token中，服务器响应时快时慢 ':-^46}")
+        print(f"\n{' 获取必要参数 ':-^53}\n")
         time_stamp = round(time() * 1000)
         auth_key = md5(f'testtest{time_stamp}'.encode('utf-8')).hexdigest()
         auth_data = {'authKey': auth_key, 'timeStamp': time_stamp}
         token_request = self.post_tool(self.auth_url, auth_data, "", self.base_header)
         token_result = token_request.json()['params']['bussiness']
         self.base_header.update({'Token': token_result, 'Accept':'application/json'})
-        print(f"\n{' 已获得Token ':-^56}\n")
 
 class file_tool(object):
     """
@@ -71,7 +71,7 @@ class file_tool(object):
                 self.file_content = f.read()
                 return self.file_content
 
-class query_info(get_require_info, file_tool):
+class query_info(get_required_info, file_tool):
     """
     输入过滤，只对中文输入做过滤，英文域名由于有些域名不在可备案列表内也能备案，所以不做过滤了
     """
@@ -82,18 +82,22 @@ class query_info(get_require_info, file_tool):
     def regular_input(self):
         print(f"{' 输入 域名/备案号/完整公司名 以查询备案信息 ':-^40}\n")
         info_input = input('查询对象：').replace(' ', '').replace('https://www.', '').replace('http://www.', '').replace('http://', '').replace('www.', '')
-        if info_input == '':
-             print(f"\n{' 请输入正确域名 ':*^52}\n")
-             return -1
-        elif '\u4e00' <= info_input <= '\u9fff':
+        if '\u4e00' <= info_input <= '\u9fff':
             input_result = re.sub('[^\\u4e00-\\u9fff-A-Za-z0-9,-.()《》—（）]', "", info_input) # 中文输入混入的特殊符号，只允许：-—《》（）().
-        else:
+        elif '.' in info_input:
             input_result = info_input
-        self.query_text = {'pageNum': 1, 'pageSize': 40, 'serviceType': 1, 'unitName': input_result} #serviceType: 1:网站 6:APP 7:小程序 8:快应用
+        elif info_input == '' or '.' not in info_input: # 空值和只输入英文字母没有域名的.则返回错误
+            print(f"\n{' 请输入正确域名 ':*^52}\n")
+            return -1
+        self.query_text = {'pageNum': 1, 'pageSize': 100, 'serviceType': 1, 'unitName': input_result} #serviceType: 1:网站 6:APP 7:小程序 8:快应用
 
     def query_information(self):
         print(f"\n{' 查询中 ':-^56}")
-        icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+        try:
+            icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+        except:
+            print(f"{' 查询出错，请重试 ':-^51}\n")
+            return -1
         total_domains = icp_info['total']
         total_pages = icp_info['lastPage']
         end_row = icp_info['endRow']
@@ -124,7 +128,11 @@ class query_info(get_require_info, file_tool):
             if icp_info['isLastPage'] is True:
                 break
             else:
-                icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+                try:
+                    icp_info = super().post_tool(self.query_url, '', self.query_text, self.base_header).json()['params']
+                except:
+                    print(f"{' 查询出错，请重试 ':-^51}\n")
+                    return -1
                 end_row = icp_info['endRow']
                 sleep(3) # 翻页需要慢一点，否则大概率被WAF拦截
 
@@ -182,19 +190,21 @@ class excel_tool(query_info):
         try:
             self.work_book.save(self.excel_file_path)
         except PermissionError:
-            return print("** 备案信息登记表格已打开，无法写入文件。如需写入，请关闭文件后重新执行！ **\n")
+            print("** 备案信息登记表格已打开，请关闭后按任意键执行录入！ **\n")
+            os.system('pause')
+            self.set_format()
         return print(f"{' 查询结果保存在：' + str(self.excel_file_path) + ' ':*^47}\n")
 
 if __name__ == '__main__':
    """
-   各类可预期的报错，返回为-1
+   各类可预期的报错，返回为 -1
    """
    os.environ['no_proxy'] = '*'
    query_target = query_info() 
-   requirement_info = get_require_info() 
+   requirement_info = get_required_info() 
    save_data = excel_tool() 
    print(f"\n{' 项目地址：https://github.com/wongzeon/ICP-Checker ':-^54}\n")
-   print(f"{' V2.2.0 2023/11/23 仅用于学习研究，不得用于商业/非法用途 ':-^41}")
+   print(f"{' V2.2.1 2023/11/23 仅用于学习研究，不得用于商业/非法用途 ':-^41}")
    requirement_info.get_cookie() 
    token = requirement_info.get_token() 
    exe_num = 0
